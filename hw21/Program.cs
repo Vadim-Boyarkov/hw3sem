ab9069f938def98e56f58f36ae82faaa79436eac
boyarkov_v <fluentzhiv@gmail.com>
2022-09-09 14:09:31 +0400

    дз готово

---
+++ hw21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координату X первой точки");
+double coordX1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y первой точки");
+double coordY1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z первой точки");
+double coordZ1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату X второй точки");
+double coordX2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y второй точки");
+double coordY2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Z второй точки");
+double coordZ2 = Convert.ToInt32(Console.ReadLine());
+
+double qwx = Math.Pow((coordX2 - coordX1), 2);
+
+double qwy = Math.Pow((coordY2 - coordY1), 2);
+double qwz = Math.Pow((coordZ2 - coordZ1), 2);
+double length = Math.Sqrt(qwx + qwy + qwz);
+Console.WriteLine(Math.Round(length , 2));
---
+++ hw23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("введите любое положительное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+int i = 1;
+while ( i <= number )
+{
+Console.Write(Math. Pow(i, 3) +" ");
+i++;
+}
+}
+else
+{
+Console.WriteLine("вы ввели не положительное число");
+}
